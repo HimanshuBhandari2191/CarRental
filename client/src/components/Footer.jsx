831e11e5113ea4c2a225f747e3add647e7950a5a
Himanshu Bhandari <himanshubhandari2191@gmail.com>
2026-04-15 23:28:52 +0530

    Update footer credit to reflect author name

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -84,7 +84,7 @@ const Footer = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{  duration: 0.6 , delay:0.6}}
             className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
-                <p>© {new Date().getFullYear()} <a href="https://shailesh-ss.netlify.app/">Car Rental by shailesh</a>. All rights reserved.</p>
+                <p>© {new Date().getFullYear()} <a href="">Car Rental by Himanshu</a>. All rights reserved.</p>
                 <ul className='flex items-center gap-4'>
                     <li><a href="#">Privacy</a></li>
                     <li>|</li>
